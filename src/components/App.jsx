50d72357be4ca6e29a509cd4e44be570edab8f11
YanNaingYan <yannnai2711@gmail.com>
2023-12-14 21:07:56 +0630

    footer added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Gallery from './Gallery'
 import Location from './Location'
+import Footer from './Footer'
 const App = () => {
   return (
     <div>
       < Gallery />
       < Location />
+      < Footer />
     </div>
   )
 }
