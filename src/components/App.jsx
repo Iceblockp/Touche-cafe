70591ac537b218adc1d4ea7a5056906192b97ffb
Iceblockp <phonyo126@gmail.com>
2023-12-14 22:32:14 +0630

    letter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Gallery from './Gallery'
+import Newsletter from './Newsletter'
 
 const App = () => {
   return (
     <div>
-      < Gallery />
+      < Newsletter />
     </div>
   )
 }
