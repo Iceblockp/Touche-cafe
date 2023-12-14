72f6b6b7405f7f2f356cd2edd3667d2dcf13c0a7
Iceblockp <phonyo126@gmail.com>
2023-12-14 16:54:55 +0630

    online reservation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import OnlineReservation from './OnlineReservation'
 
 const App = () => {
   return (
     <div>
-      App
+      <OnlineReservation/>
     </div>
   )
 }
