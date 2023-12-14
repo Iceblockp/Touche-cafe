ec317d235e2c05e28c433d8ec5b4cf8b54ac61d0
YanNaingYan <yannnai2711@gmail.com>
2023-12-14 16:36:51 +0630

    gallery section added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Gallery from './Gallery'
 
 const App = () => {
   return (
     <div>
-      App
+      < Gallery />
     </div>
   )
 }
---
+++ src/components/GalleryPhotos.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import gallery1 from '../image/gallery1.jpg';
+import gallery2 from '../image/gallery2.jpg';
+import gallery3 from '../image/gallery3.jpg';
+import gallery4 from '../image/gallery4.jpg';
+import gallery5 from '../image/gallery5.jpg';
+import gallery6 from '../image/gallery6.jpg';
+
+import gallery7 from '../image/gallery7.jpg';
+import gallery8 from '../image/gallery8.jpg';
+
+
+
+const GalleryPhotos = () => {
+  return (
+    <div className='  grid lg:grid-cols-3 sm:grid-cols-2  gap-4 '>
+<div className='  '>
+<img src={gallery1} alt="" />
+</div>
+<div className=' row-span-2'>
+<img src={gallery2} alt="" />
+</div>
+<div className='lg:mt-0 sm:-mt-20'>
+<img src={gallery3} alt="" />
+</div>
+<div className='lg:-mt-10 sm:-mt-20'>
+<img src={gallery4} alt="" />
+</div>
+<div className=''>
+<img src={gallery5} alt="" />
+</div>
+<div className='lg:-mt-16 sm:-mt-28'>
+<img src={gallery6} alt="" />
+</div>
+<div className=''>
+<img src={gallery7} alt="" />
+</div>
+<div className='lg:mt-0 sm:-mt-24'>
+<img src={gallery8} alt="" />
+</div>
+
+    </div>
+  )
+}
+
+export default GalleryPhotos
