f99668753dacd50c17052de00300578d5f7a5509
HtutMyatLinn <htutmyatlinnhml18@gmail.com>
2023-12-14 10:22:06 +0630

    header2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='mt-7'>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+// import Hero form "../image/"
+
+const Header = () => {
+  return (
+    <div id="header" className=" container mx-auto">
+      <div className="flex justify-between ">
+        <div className=" flex  gap-4 mr-52">
+          <a className="  p-4 " href="">
+            ABOUT US
+          </a>
+          <a className="  p-4 " href="#MENU">
+            MENU
+          </a>
+          <a className="  p-4 " href="#RESERVATION">
+            RESERVATION
+          </a>
+        </div>
+        <a className=" font-heading p-4 " href="#Touche">
+          Touche
+        </a>
+        <div className=" flex  gap-4 ml-52">
+          <a className="  p-4 " href="#NEWS & EVENTS">
+            NEWS & EVENTS
+          </a>
+          <a className="  p-4 " href="#GALLERY">
+            GALLERY
+          </a>
+          <a className="  p-4 " href="#CONTACT">
+            CONTACT
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
