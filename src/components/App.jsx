86ced93d7b5f9282e4ae7462e7b5cd257776121c
Iceblockp <phonyo126@gmail.com>
2023-12-14 21:14:16 +0630

    event finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Gallery from './Gallery'
+import EventShow from './EventShow'
 
 const App = () => {
   return (
     <div>
-      < Gallery />
+      <EventShow/>
     </div>
   )
 }
