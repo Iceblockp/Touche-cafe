b82aab282b4a7c80b7e24e5b9c57388b6e2cf684
Iceblockp <phonyo126@gmail.com>
2023-12-14 18:58:31 +0630

    about finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Gallery from './Gallery'
+import AboutUs from './AboutUs'
 
 const App = () => {
   return (
     <div>
-      < Gallery />
+      <AboutUs/>
     </div>
   )
 }
