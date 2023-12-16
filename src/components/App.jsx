90ecfa5d85b47005aff8ed52a7fb1c978621a30a
Iceblockp <phonyo126@gmail.com>
2023-12-16 22:09:59 +0630

    menu error done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import Menu from './Menu'
+
 import OnlineReservation from "./OnlineReservation";
 import Dishes from "./Dishes";
 import SectionThree from "./SectionThree";
@@ -9,29 +9,25 @@ import Newsletter from "./Newsletter";
 import EventShow from "./EventShow";
 import AboutUs from "./AboutUs";
 import SectionFour from "./SectionFour";
-import Menu from "./Menu";
+
 import SectionTwo from "./sectionTwo";
 import Location from "./Location";
-import Footer from "./Footer";
+
 import NavBar from "./NavBar";
 import TopHeader from "./TopHeader";
+import Menu from './Menu';
+import Footer from './Footer';
 
 
 const App = () => {
   return (
     <div>
-
-      
-      <Menu/>
-
-
       <TopHeader/>
       <NavBar />
       <SectionTwo />
-
       <SectionThree />
       <SectionFour />
-      <Menu />
+      <Menu/>
       <Dishes />
       <OnlineReservation />
       <AboutUs />
@@ -39,7 +35,8 @@ const App = () => {
       <Newsletter />
       <Gallery />
       <Location />
-      <Footer />
+      <Footer/>
+     
 
     </div>
   );
