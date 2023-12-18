5a94e84c61fc58d969e789484724a17b1237498b
HtutMyatLinn <htutmyatlinnhml18@gmail.com>
2023-12-18 20:39:54 +0630

    header center done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,8 @@ import Title from "./Title";
 
 const Header = () => {
   return (
-    <div id="header" className=" w-10/12 mx-auto relative">
-      
-
-      <div className=" text-white">
+    <div id="header" className=" w-10/12 mx-auto relative h-full">
+      <div className=" text-white h-full">
         <Title />
       </div>
     </div>
