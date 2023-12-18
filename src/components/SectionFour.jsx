8ffca356060ef43e0c9f283855a7e799949a6402
Iceblockp <phonyo126@gmail.com>
2023-12-18 19:41:49 +0630

    section four error done

--- src/components/SectionFour.jsx
+++ src/components/SectionFour.jsx
@@ -4,20 +4,13 @@ import coffeeBottle from "../image/coffeeBottle.jpg";
 const SectionFour = () => {
   return (
     <section className="py-7  ">
-      <div className="relative lg:mb-[250px] md:mb-[350px] sm:mb-[450px]">
+      <div className="relative ">
         {/* <img
           className="absolute w-full h-[780px] md:h-[580px] "
           src={coffeeBottle}
           alt=""
         /> */}
-        <div
-          className=" absolute w-full h-[480px] bg-cover bg-no-repeat  "
-          style={{
-            backgroundImage: `url(${coffeeBottle})`,
-            backgroundPosition: "50% 60%",
-          }}
-        ></div>
-        <div className="container relative bg-gradient-to-b from-white to-transparent">
+         <div className=" z-30 w-full absolute bg-gradient-to-b from-white to-transparent">
           <div className="flex items-center md:py-9">
             <div className=" w-10/12 mx-auto pl-4 mt-10 ">
               <hr className=" relative w-8 h-[3px] mb-3 bg-[#f5a637]" />
@@ -28,6 +21,14 @@ const SectionFour = () => {
             </div>
           </div>
         </div>
+        <div
+          className=" relative w-full h-[480px] bg-cover bg-no-repeat  "
+          style={{
+            backgroundImage: `url(${coffeeBottle})`,
+            backgroundPosition: "50% 60%",
+          }}
+        ></div>
+       
       </div>
     </section>
   );
