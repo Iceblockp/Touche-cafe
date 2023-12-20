1f7dfe8757f91bde2cfa909eb48cb35f489100d3
Iceblockp <phonyo126@gmail.com>
2023-12-20 14:46:50 +0630

    menu responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ export default {
     fontFamily: {
       'sans': ['Lato', 'sans-serif'],
     },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
 
     extend: {
       fontFamily: {
